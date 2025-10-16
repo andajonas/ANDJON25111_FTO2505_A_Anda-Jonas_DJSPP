97331cbe174e01e019595ade362a35fdb0d5cec0
andajonas <andajonas63@gmail.com>
2025-10-16 21:51:00 +0200

    create homepage with carousel and filters

--- .history/src/Pages/HomePage_20251009170003.jsx
+++ .history/src/Pages/HomePage_20251009170003.jsx
@@ -2,26 +2,30 @@ import SearchBar from "../components/SearchBar";
 import SortSelect from "../components/SortSelect";
 import GenreFilter from "../components/GenreFilter";
 import PodcastGrid from "../components/PodcastGrid";
-import ShowCarousel from "../components/ShowCarousel/ShowCarousel";
 import Pagination from "../components/Pagination";
 import styles from "./HomePage.module.css";
 
-export default function HomePage({ genres, recommendedShows }) {
+/**
+ * HomePage Component
+ * 
+ * Renders the main podcast browsing interface with search, filters,
+ * and paginated podcast grid.
+ * 
+ * @component
+ * @param {Object} props - Component props
+ * @param {{id: number, name: string}[]} props.genres - Array of genre definitions
+ * 
+ * @returns {JSX.Element} The home page layout
+ */
+export default function HomePage({ genres }) {
   return (
     <>
-      {/* Recommended Shows Carousel */}
-      {recommendedShows && recommendedShows.length > 0 && (
-        <ShowCarousel shows={recommendedShows} genres={genres} />
-      )}
-
-      {/* Search and Filters */}
       <section className={styles.controls}>
         <SearchBar />
         <GenreFilter genres={genres} />
         <SortSelect />
       </section>
 
-      {/* Podcast Grid */}
       <PodcastGrid genres={genres} />
       <Pagination />
     </>
